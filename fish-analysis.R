12074b557f8557aef45dc072ba9b367f6613bcb9
JMcInnes87 <78825240+JMcInnes87@users.noreply.github.com>
2021-02-10 15:26:19 -0800

    Move data file into subdirectory

--- fish-analysis.R
+++ fish-analysis.R
@@ -5,3 +5,4 @@ fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
