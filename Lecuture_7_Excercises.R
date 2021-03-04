bc94f73f93bcad89ac01e45f50ecbd6518fb8ab4
JMcInnes87 <78825240+JMcInnes87@users.noreply.github.com>
2021-03-04 15:52:28 -0800

    Weight conversion pounds to grams

---
+++ Lecuture_7_Excercises.R
@@ -0,0 +1,12 @@
+#Lecture 7
+#Excercise 1
+
+#Convert weight in pounds to grams
+pounds_to_grams <- function(weight) {
+weight_in_grams <- weight * 453.592
+return(weight_in_grams)
+}
+#Use that function to calculate how many grams there are in 3.75 pounds.
+pounds_to_grams(3.75)
+[1] 1700.97
+
