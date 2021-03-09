22b4a5c2ea3aeb3d8228d518fd31512b0a784b17
JMcInnes87 <78825240+JMcInnes87@users.noreply.github.com>
2021-03-09 15:47:03 -0800

    Excercise 1 problems

---
+++ Lecture_8.R
@@ -0,0 +1,23 @@
+#Exercise 1
+#Create the following variables
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+#1.w is greater than 10
+w > 10
+#True
+#2. "green" is in colors
+"green"%in% c("red", "blue", "green")
+#True
+#3. x is greater than y
+x > y
+#FALSE
+#4. Each value in masses is greater than 40.
+mass > 40
+#True
